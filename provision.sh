1b4a19375ea871717f721de172eb32398a873eb4
Anastasiia Nidzelska <39418013+nidzelska@users.noreply.github.com>
2018-07-12 17:11:05 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -47,6 +47,7 @@ sudo cp /vagrant/ansible/hosts /etc/ansible/
 
 #Install Artifactory
 sudo apt-get install unzip > /dev/null 2>&1
+cd /
 sudo wget https://bintray.com/jfrog/artifactory/download_file?file_path=jfrog-artifactory-oss-5.1.3.zip
 sudo unzip download_file?file_path=jfrog-artifactory-oss-5.1.3.zip
 sudo artifactory-oss-5.1.3/bin/artifactoryctl start
